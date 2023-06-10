edbfd809f76fe24310f5043551b8d902a578f031
angpace <108838753+angpace@users.noreply.github.com>
2023-06-10 19:56:49 -0400

    Adjusting portfolio

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -2,54 +2,54 @@ import React from 'react'
 import './portfolio.css'
 import IMG1 from '../../assets/portfolio1.jpg'
 import IMG2 from '../../assets/portfolio2.jpg'
-import IMG3 from '../../assets/portfolio3.jpg'
-import IMG4 from '../../assets/portfolio4.jpg'
-import IMG5 from '../../assets/portfolio5.png'
-import IMG6 from '../../assets/portfolio6.jpg'
+import IMG3 from '../../assets/privee.gif'
+import IMG4 from '../../assets/NYClean.gif'
+import IMG5 from '../../assets/Synodic.gif'
+import IMG6 from '../../assets/Smoothie.gif'
 
 // DO NOT USE THE IMAGES IN PRODUCTION
 
 const data = [
   {
     id: 1,
-    image: IMG1,
-    title: 'Crypto Currency Dashboard & Financial Visualization',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/16673715-Crypto-currency-dashboards-and-financial-data-visualization'
+    image: IMG2,
+    title: 'An e-commerce site for Foglia Brand. In progress',
+    github: 'https://github.com/angpace/foglia',
+    demo: 'https://dribbble.com/shots/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
   },
   {
     id: 2,
-    image: IMG2,
-    title: 'Charts templates & infographics in Figma',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/16580766-Orion-UI-kit-Charts-templates-infographics-in-Figma'
+    image: IMG1,
+    title: 'Kitchen and Code, a blog about food and tech.',
+    github: 'https://github.com/angpace/kitchenandcode',
+    demo: 'https://www.kitchenandcode.com/'
   },
   {
     id: 3,
     image: IMG3,
-    title: 'Figma dashboard UI kit for data design web apps',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/17290917-Eclipse-Figma-dashboard-UI-kit-for-data-design-web-apps'
+    title: 'Privee, a food platform connecting Clients and Chefs.',
+    github: 'https://github.com/angpace/privee',
+    demo: 'https://youtu.be/NrPr2wIKGgA'
   },
   {
     id: 4,
     image: IMG4,
-    title: 'Maintaining tasks and tracking progress',
-    github: 'https://github.com',
-    demo: 'https://dribbble.com/shots/16955822-Maintaining-tasks-and-tracking-progress'
+    title: 'NYCleans, a \'non-profit\', volunteer based cleaning app.',
+    github: 'https://github.com/B-Keshav/NYCleans',
+    demo: 'https://www.youtube.com/watch?v=RHoJySoRt-8&feature=youtu.be'
   },
   {
     id: 5,
     image: IMG5,
-    title: 'Charts templates & infographics in Figma',
-    github: 'https://github.com',
+    title: 'Synodic',
+    github: 'https://github.com/angpace/Synodic',
     demo: 'https://dribbble.com/shots/16541289-Orion-UI-kit-Charts-templates-infographics-in-Figma'
   },
   {
     id: 6,
     image: IMG6,
-    title: 'Charts templates & infographics in Figma',
-    github: 'https://github.com',
+    title: 'SmoothieMaster',
+    github: 'https://github.com/angpace/smoothiemaster',
     demo: 'https://dribbble.com/shots/15887665-Orion-UI-kit-Charts-templates-infographics-in-Figma'
   }
 ]
