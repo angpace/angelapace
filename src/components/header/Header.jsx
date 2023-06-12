ebb919b0076683c0138a5f4fcf95e64ab4939a69
angpace <108838753+angpace@users.noreply.github.com>
2023-06-12 12:49:49 -0400

    Changing theme- removing white and adding WebP instead of png for Header image

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import './header.css'
 import CTA from './CTA'
-import ME from '../../assets/mee.png'
+import ME from '../../assets/me.WebP'
 import HeaderSocial from './HeaderSocials'
 
 const Header = () => {
